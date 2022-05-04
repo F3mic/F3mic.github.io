306229808a9a6dfd4ea2166c022c78878f801b5c
F3mic <vault0citizen@gmail.com>
2022-05-04 15:05:14 +0200

    added javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(){
+    if ($(window).scroll( function(){
+        $('.item-sidebar').fadeOut($(this) > 250);
+    }));
+
+    else ($(window).scroll( function(){
+        $('.item-sidebar').fadeIn($(this) < 250);
+    }));      
+});
+*/
