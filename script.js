6bb1ccadf62dc2541ce1a13fbec4cfae2457ff34
F3mic <vault0citizen@gmail.com>
2022-05-05 16:50:14 +0200

    added javascript

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /*
 $(document).ready(function(){
     if ($(window).scroll( function(){
-        $('.item-sidebar').fadeOut($(this) > 250);
+        $('.item-sidebar').fadeOut($(this) > 1);
     }));
 
     else ($(window).scroll( function(){
-        $('.item-sidebar').fadeIn($(this) < 250);
+        $('.item-sidebar').fadeIn($(this) < 1);
     }));      
 });
 */
+data-aos="slide-right"
+data-aos-anchor="#trigger-left"
+data-aos-anchor-placement="top-top"
